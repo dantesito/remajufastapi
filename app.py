0ad0c6ffae6764407fbdec27d4f0b7e0a1790924
dantesito <ingdantev@hotmail.com>
2025-02-20 13:14:25 -0500

    change package

--- app.py
+++ app.py
@@ -1,24 +1,5 @@
-from fastapi import FastAPI
-
-schema_get_captcha = {
-    "name": "lista productos",
-    "baseSelector": ".product-item",
-    "fields": [
-
-        {
-            "name": "nombre",
-            "selector": ".product-info .product-name",
-            "type": "text"
-        }
-
-    ]
-}
-app = FastAPI()
-async def main():
-   return {"say":"hola"}
-
-#if __name__ == "__main__":
-#    asyncio.run(main())
-@app.get("/")
-async def main():
-  return await main()
+from flask import Flask, request
+app = Flask(__name__)
+@app.route("/")
+async def hello():
+    return {"ko":"der"}
